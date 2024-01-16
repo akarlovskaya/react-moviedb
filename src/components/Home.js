2a03cf67282685dfb7b3e1223bb0bef1dff31059
Anna Karlovskaya <annakarlovskaya@Annas-MacBook-Air.local>
2024-01-15 16:48:27 -0800

    add Grid and Thumb

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ import API from '../API';
 import {  IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../config';
 // components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -20,15 +22,30 @@ const Home = () => {
 
     return (
         <>
-            { state.results[0] ?
+            { state.results[0] ? (
                  <HeroImage 
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title={state.results[0].original_title}
                     text={state.results[0].overview}
                  />
-            : null
-            }
-       
+            ) : null }
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <Thumb 
+                    key={movie.id}
+                    clickable={true}
+                    image={
+                        movie.poster_path
+                          ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                          : NoImage
+                      }
+                      movieId={movie.id}
+                    
+                    />
+
+                ))}
+
+            </Grid>
         </>
     )
 }
