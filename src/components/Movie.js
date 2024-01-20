be7b35b79f8bbe80d8512d5eecd094048fd93f16
Anna Karlovskaya <annakarlovskaya@Annas-MacBook-Air.local>
2024-01-19 19:41:38 -0800

    get movie, create add useMovieFetch

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,36 @@
 import React from 'react';
+// to get moveiId from router
+import { useParams } from 'react-router-dom';
 
-const Movie = () => <div>Movie</div>;
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+// import BreadCrumb from './BreadCrumb';
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hook
+import {useMovieFetch} from '../hooks/useMovieFetch';
+
+// Fallback Image
+import NoImage from '../images/no_image.jpg';
+import API from '../API';
+
+const Movie = () => {
+    // get movieId from Route path='/:movieId'
+    const { movieId } = useParams();
+    // rename state -> movie like this state: movie
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log('movie from API: ', movie);
+
+    return (
+        <>
+            <div>Movie</div>;
+        </>
+    )
+}
+
+
 
 export default Movie;
